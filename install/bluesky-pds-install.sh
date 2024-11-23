f3a1707827bf92323f083b5858ab340df9b23d4a
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2024-11-22 23:39:16 -0500

    add pds.env

--- install/bluesky-pds-install.sh
+++ install/bluesky-pds-install.sh
@@ -17,6 +17,8 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
+$STD apt-get install -y sqlite3
+$STD apt-get install -y openssl
 msg_ok "Installed Dependencies"
 
 get_latest_release() {
@@ -25,7 +27,17 @@ get_latest_release() {
 
 DOCKER_LATEST_VERSION=$(get_latest_release "moby/moby")
 BLUESKYPDS_LATEST_VERSION="0.4"
+PDS_DID_PLC_URL="https://plc.directory"
+PDS_BSKY_APP_VIEW_URL="https://api.bsky.app"
+PDS_BSKY_APP_VIEW_DID="did:web:api.bsky.app"
+PDS_REPORT_SERVICE_URL="https://mod.bsky.app"
+PDS_REPORT_SERVICE_DID="did:plc:ar7c4by46qjdydhdevvrndac"
+PDS_CRAWLERS="https://bsky.network"
 PDSADMIN_URL="https://raw.githubusercontent.com/bluesky-social/pds/main/pdsadmin.sh"
+GENERATE_SECURE_SECRET_CMD="openssl rand --hex 16"
+GENERATE_K256_PRIVATE_KEY_CMD="openssl ecparam --name secp256k1 --genkey --noout --outform DER | tail --bytes=+8 | head --bytes=32 | xxd --plain --cols 32"
+PDS_ADMIN_PASSWORD=$(eval "${GENERATE_SECURE_SECRET_CMD}")
+
 
 msg_info "Installing Docker $DOCKER_LATEST_VERSION"
 DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
@@ -39,12 +51,28 @@ $STD docker pull ghcr.io/bluesky-social/pds:latest
 msg_ok "Pulled BlueSky PDS $BLUESKYPDS_LATEST_VERSION Image"
 
 msg_info "Installing BlueSky PDS $BLUESKYPDS_LATEST_VERSION"
-PDS_PATH='/pds'
-mkdir -p $(dirname $DOCKER_CONFIG_PATH)
-$STD docker run -d --name pds --network host --restart unless-stopped -v /pds:/pds ghcr.io/bluesky-social/pds:latest
 
+$STD mkdir "/root/pds"
 
-# msg_info "Creating PDS Service"
+cat <<PDS_CONFIG >"/root/pds/pds.env"
+PDS_HOSTNAME=${PDS_HOSTNAME}
+PDS_JWT_SECRET=$(eval "${GENERATE_SECURE_SECRET_CMD}")
+PDS_ADMIN_PASSWORD=${PDS_ADMIN_PASSWORD}
+PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX=$(eval "${GENERATE_K256_PRIVATE_KEY_CMD}")
+PDS_DATA_DIRECTORY=/root/pds/blocks
+PDS_BLOBSTORE_DISK_LOCATION=/root/pds/blocks
+PDS_BLOB_UPLOAD_LIMIT=52428800
+PDS_DID_PLC_URL=${PDS_DID_PLC_URL}
+PDS_BSKY_APP_VIEW_URL=${PDS_BSKY_APP_VIEW_URL}
+PDS_BSKY_APP_VIEW_DID=${PDS_BSKY_APP_VIEW_DID}
+PDS_REPORT_SERVICE_URL=${PDS_REPORT_SERVICE_URL}
+PDS_REPORT_SERVICE_DID=${PDS_REPORT_SERVICE_DID}
+PDS_CRAWLERS=${PDS_CRAWLERS}
+LOG_ENABLED=true
+PDS_CONFIG
+
+$STD docker run -d --name pds --network host --restart unless-stopped -v /pds:/pds -env_file /pds/pds.env ghcr.io/bluesky-social/pds:latest
+
 # cat <<EOF >/etc/systemd/system/pds.service
 # [Unit]
 # Description=Bluesky PDS Service
@@ -54,7 +82,7 @@ $STD docker run -d --name pds --network host --restart unless-stopped -v /pds:/p
 # Type=oneshot
 # RemainAfterExit=yes
 # WorkingDirectory=/root/pds
-# ExecStart=/usr/bin/docker run -d --name pds --network host --restart unless-stopped -v /pds:/pds ghcr.io/bluesky-social/pds:latest
+# ExecStart=/usr/bin/docker run -d --name pds --network host --restart unless-stopped -v /pds:/pds -env_file /pds/pds.env ghcr.io/bluesky-social/pds:latest
 # ExecStop=/usr/bin/docker stop pds
 # Restart=always
 
@@ -64,7 +92,6 @@ $STD docker run -d --name pds --network host --restart unless-stopped -v /pds:/p
 # systemctl daemon-reload
 # systemctl enable -q --now pds
 msg_ok "Installed BlueSky PDS $BLUESKYPDS_LATEST_VERSION"
-# msg_ok "Created PDS Service"
 
 msg_info "Downloading pdsadmin tool"
 curl -sfSo /usr/local/bin/pdsadmin "${PDSADMIN_URL}"
